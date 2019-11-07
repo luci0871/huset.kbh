2b457ac568b80ebb13dac6e82dfcf9f58e8dc53d
luci0871 <luci0871@stud.kea.dk>
2019-11-07 23:33:53 +0100

    added sub pages

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-window.addEventListener "(DOMContentLoaded", getData);
+/*window.addEventListener "(DOMContentLoaded", getData);
 
 function getData() {
     console.log("getData")
@@ -15,4 +15,52 @@ function handleData(myData) {
 
 function showPost(post) {
     console.log(post)
+}
+*/
+
+window.addEventListener("DOMContentLoaded", getData);
+
+function getData() {
+    //console.log("getData")
+    fetch("http://dredesigns.dk/MyWordpress/wp-json/wp/v2/films")
+        .then(res => res.json())
+        .then(useData)
+}
+
+function useData(myData) {
+    //console.log(myData)
+
+    //1- Loop the array
+    myData.forEach(showEvent)
+}
+
+function showEvent(event) {
+    console.log(event)
+    //2- Clone the template
+    //image
+    /*const imgPath = event._embedded["wp:featuredmedia"][0].media_details.sizes.thumbnail.source_url;*/
+
+    const template = document.querySelector(".filmTemplate").content;
+    const eventCopy = template.cloneNode(true);
+    //3- textcontent and ineer HTML
+    const h1 = eventCopy.querySelector("h1");
+    h1.textContent = event.title.rendered;
+
+    const prices = eventCopy.querySelector(".prices");
+    prices.innerHTML = event.price;
+
+
+
+    /*image
+    const img = eventCopy.querySelector("img.cover");
+    img.setAttribute("src", imgPath)
+    img.setAttribute("alt", "Cover of the book" + event.title.rendered)*/
+
+    //subpage
+    const a = eventCopy.querySelector("a");
+    a.href = "sub.html?id=" + event.id
+
+    //4- Append
+    document.querySelector("#event").appendChild(eventCopy);
+
 }
