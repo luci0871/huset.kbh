af6ac1d53f44b6e6b56f0978a0cc23e6324b8d28
luci0871 <luci0871@stud.kea.dk>
2019-11-07 11:57:26 +0100

    added javascript info

--- javascript.js
+++ javascript.js
@@ -1,3 +1,18 @@
 window.addEventListener "(DOMContentLoaded", getData);
 
-function getData
+function getData() {
+    console.log("getData")
+    fetch("http://dredesigns.dk/MyWordpress/wp-json/wp/v2/films")
+        .then(res => res.json())
+        .then(handleData)
+}
+
+function handleData(myData) {
+    //console.log(myData);
+    //1 loop
+    myData.forEach(showPost)
+}
+
+function showPost(post) {
+    console.log(post)
+}
