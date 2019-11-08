2cdbe7d2cf55fd8fa96a64c1c1c4913bedf924b3
luci0871 <luci0871@stud.kea.dk>
2019-11-08 10:29:49 +0100

    added title and long description

---
+++ subscript.js
@@ -0,0 +1,14 @@
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("id");
+console.log(id)
+
+fetch("http://dredesigns.dk/MyWordpress/wp-json/wp/v2/films/" + id)
+    .then(res => res.json())
+    .then(showEvent)
+
+function showEvent(event) {
+    console.log(event)
+    document.querySelector("article h1").textContent = event.title.rendered;
+
+    document.querySelector(".longdescription").textContent = event.content.rendered;
+}
