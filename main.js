3b12ef0e2bd8bd95b832159be95d72d19ca0b478
luci0871 <luci0871@stud.kea.dk>
2019-11-13 11:19:01 +0100

    added nav, serach on javascript is working

--- main.js
+++ main.js
@@ -1,40 +1,32 @@
-/*window.addEventListener "(DOMContentLoaded", getData);
-
-function getData() {
-    console.log("getData")
-    fetch("http://dredesigns.dk/MyWordpress/wp-json/wp/v2/films")
-        .then(res => res.json())
-        .then(handleData)
-}
-
-function handleData(myData) {
-    //console.log(myData);
-    //1 loop
-    myData.forEach(showPost)
-}
-
-function showPost(post) {
-    console.log(post)
-}
-*/
-
 window.addEventListener("DOMContentLoaded", getData);
 
 //const urlParams = new URLSearchParams(window.location.search);
 //const search = urlParams.get("search");
+
 function getData() {
     const urlParams = new URLSearchParams(window.location.search);
     const search = urlParams.get("search");
     const id = urlParams.get("id");
-    if (search) {
-        console.log("this is a search result")
-    } else {
-        console.log("not searching")
-    }
 
+    if (search) {
+        //console.log("this is a search result")
+        getSearchData();
+    } else if (id) {
+        getSingleEvent();
+    } else {
+        //console.log("not searching")
+        getFrontpageData();
+    }
+    //getNavigation()
 }
 
-
+/*function getNavigation() {
+    console.log(getNavigation)
+    fetch("http://dredesigns.dk/MyWordpress/wp-json/wp/v2/categories?")
+        .then(res => res.json())
+        .then(data => {})
+}
+*/
 function getSearchData() {
     const urlParams = new URLSearchParams(window.location.search);
     const search = urlParams.get("search");
@@ -46,13 +38,32 @@ function getSearchData() {
         .then(useData)
 }
 
+function getSingleEvent() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get("id");
+    console.log(getSingleEvent)
+
+    fetch("http://dredesigns.dk/MyWordpress/wp-json/wp/v2/films/" + id)
+        .then(res => res.json())
+        .then(showEvent)
+}
+
 function getFrontpageData() {
     //console.log("getData")
-    fetch("http://dredesigns.dk/MyWordpress/wp-json/wp/v2/films")
+    fetch("http://dredesigns.dk/MyWordpress/wp-json/wp/v2/films/")
         .then(res => res.json())
         .then(useData)
 }
 
+
+
+/*function showEvent(event) {
+    console.log(event)
+    document.querySelector("article h1").textContent = event.title.rendered;
+
+    document.querySelector(".longdescription").textContent = event.content.rendered;
+}
+*/
 function useData(myData) {
     //console.log(myData)
 
@@ -66,11 +77,14 @@ function showEvent(event) {
     //image
     /*const imgPath = event._embedded["wp:featuredmedia"][0].media_details.sizes.thumbnail.source_url;*/
 
-    const template = document.querySelector(".filmTemplate").content;
+    const template = document.querySelector("template").content;
     const eventCopy = template.cloneNode(true);
+    console.log(eventCopy);
+
     //3- textcontent and ineer HTML
-    const h1 = eventCopy.querySelector("h1");
+    const h1 = eventCopy.querySelector("h1.test");
     h1.textContent = event.title.rendered;
+    console.log(h1);
 
     const prices = eventCopy.querySelector(".prices");
     prices.innerHTML = event.price;
@@ -84,7 +98,7 @@ function showEvent(event) {
 
     //subpage
     const a = eventCopy.querySelector("a");
-    a.href = "sub.html?id=" + event.id
+    a.href = "sub.html?id=" + event.id;
 
     //4- Append
     document.querySelector("#event").appendChild(eventCopy);
