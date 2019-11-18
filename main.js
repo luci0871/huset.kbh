ce765035a597c19f2322a2b28b1c279f666766a8
luci0871 <luci0871@stud.kea.dk>
2019-11-18 13:51:10 +0100

    nav working

--- main.js
+++ main.js
@@ -21,23 +21,32 @@ function getData() {
 }
 
 function getNavigation() {
-    console.log(getNavigation)
-    fetch("http://dredesigns.dk/MyWordpress/wp-json/wp/v2/categories?_embed")
+    //console.log(getNavigation)
+    fetch("http://dredesigns.dk/MyWordpress/wp-json/wp/v2/categories?per_page=100")
+        //fetch("http://dredesigns.dk/MyWordpress/wp-json/wp/v2/films?_embed&categories")
         .then(res => res.json())
         .then(data => {
-            console.log(data)
+            //console.log(data)
             data.forEach(addLink)
         })
 }
 
 function addLink(oneItem) {
-    //console.log(oneItem);
-    // document.querySelector("nav").innerHTML=oneItem.name
-    if (oneItem.parent === 3 && oneItem.count > 0) {
+    //console.log(oneItem._embedded["wp:term"][0][0]);
+    //alert("hey")//document.querySelector("ul").innerHTML += oneItem.name
+    //if (oneItem.parent === 8 && oneItem.count > 0) {
+    if (oneItem.parent === 8) {
+        //if (oneItem.count > 0) {
+        const li = document.createElement("li");
         const link = document.createElement("a");
+
+        //link.textContent = oneItem._embedded["wp:term"][0][0].name;
         link.textContent = oneItem.name;
+        //link.setAttribute("href", "category.html?category=" + oneItem._embedded["wp:term"][0][0].id);
         link.setAttribute("href", "category.html?category=" + oneItem.id);
-        document.querySelector("ul").appendChild(link);
+
+        li.appendChild(link);
+        document.querySelector("ul").appendChild(li);
     }
 }
 
@@ -86,11 +95,11 @@ function useData(myData) {
 }
 
 function showEvent(event) {
-    console.log(event)
+    console.log(event._embedded)
     //2- Clone the template
     //image
     const imgPath = event._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url;
-
+    //console.log(imgPath);
     const template = document.querySelector("template").content;
     const eventCopy = template.cloneNode(true);
     //console.log(eventCopy);
@@ -98,7 +107,7 @@ function showEvent(event) {
     //3- textcontent and ineer HTML
     const h1 = eventCopy.querySelector("h1");
     h1.innerHTML = event.title.rendered;
-    console.log(h1);
+    //console.log(h1);
 
     const prices = eventCopy.querySelector(".prices");
     prices.innerHTML = event.price;
